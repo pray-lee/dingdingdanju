96c02ba209bfb2043fd5d840f4e4e30585f50ea9
pray-lee <praycis@gmail.com>
2020-06-01 19:55:56 +0800

    update

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ App({
     // 从后台被 scheme 重新打开
     // options.query == {number:1}
   },
-  globalData: {
-    "corpId": "ding8716f43868da3905bc961a6cb783455b",
-    "CustomKey": "suiteb21ghue9nj6gkgb0",
-    "CustomSecret": "LI8GWNTEuC4hujrgfV8ijRP41gKMlXhop2edp7A_I1jzIRaU1MakY-1pprbSTJwc"
-  }
+  // globalData: {
+  //   "corpId": "dingd6db728a5a10ba1f35c2f4657eb6378f",
+  //   "CustomKey": "suiteledp8nm95trck3th",
+  //   "CustomSecret": "pAw2w2JgcMMsF2HW7N0ik3bP0lvEvnxw_nkQRbRSBa1TQs7CbPP-hW836RoGoTAW"
+  // }
 });
