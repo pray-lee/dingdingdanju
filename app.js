31417b034b415e5e0570f0f23b200f26296d1bca
pray-lee <praycis@gmail.com>
2022-01-05 14:00:09 +0800

    update

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ App({
         // "CustomSecret": "pAw2w2JgcMMsF2HW7N0ik3bP0lvEvnxw_nkQRbRSBa1TQs7CbPP-hW836RoGoTAW",
         // ----------------------config-----------------------------
         // 内蒙古康新食品
+        agentId: '1428400011',
+        tenantCode: 'db_ck_dthd',
+        // 内蒙古康新食品
         // agentId: '1423147826',
         // tenantCode: 'db_ck_kxmc',
         // 一贝
@@ -116,15 +119,15 @@ App({
         // 测试环境进销存
         // "url": "https://www.caika.net/TestJxc/",
         // saas
-        // "url": "https://www.caika.net/saas/",
+        "url": "https://www.caika.net/saas/",
         // "url": "https://www.caika.net/caika/",
         // "url": "http://192.168.1.121:8080/jeecg/",
         // 测试环境test
         // 于龙测试
         // "url": "https://www.caika.net/jeecg/",
-        "url": "https://www.caika.net/test/",
-        "agentId": "1249138757",
-        "tenantCode": "db_ck_oa",
+        // "url": "https://www.caika.net/test/",
+        // "agentId": "1249138757",
+        // "tenantCode": "db_ck_oa",
         // 企业服务商的app
         // "agentId": '782995014'
         // 测试集团
