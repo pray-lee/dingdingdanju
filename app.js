f1e4f926d0497a8ba47e2465c3149238f3db7123
pray-lee <praycis@gmail.com>
2022-08-17 19:31:05 +0800

    update

--- app.js
+++ app.js
@@ -98,9 +98,9 @@ App({
         // "agentId": "1030661487",
         // "tenantCode": "db_ck_zhwjr",
         // 电商 (爱在当下)
-        "url": "https://www.caika.net/caika/",
-        "agentId": "886493470",
-        "tenantCode": "db_ck_ds2021",
+        // "url": "https://www.caika.net/caika/",
+        // "agentId": "886493470",
+        // "tenantCode": "db_ck_ds2021",
         // 票房宝
         // "url": "https://www.caika.net/caika/",
         // "agentId": "874431275",
@@ -143,9 +143,9 @@ App({
         // 测试环境test
         // 于龙测试
         // "url": "https://www.caika.net/jeecg/",
-        // "url": "https://www.caika.net/caika/",
-        // "agentId": "1249138757",
-        // "tenantCode": "db_ck_oa",
+        "url": "https://www.caika.net/caika/",
+        "agentId": "1249138757",
+        "tenantCode": "db_ck_oa",
         // 企业服务商的app
         // "agentId": '782995014'
         // 测试集团
