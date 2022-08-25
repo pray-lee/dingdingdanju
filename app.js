d0c644d14a9cfdfe8b60dcec24449b71140bff06
pray-lee <praycis@gmail.com>
2022-08-25 16:35:20 +0800

    update

--- app.js
+++ app.js
@@ -22,10 +22,14 @@ App({
         // "CustomKey": "suiteledp8nm95trck3th",
         // "CustomSecret": "pAw2w2JgcMMsF2HW7N0ik3bP0lvEvnxw_nkQRbRSBa1TQs7CbPP-hW836RoGoTAW",
         // ----------------------config-----------------------------
-        // 颜值时代生物科技有限公司
+        // 秦皇岛曲美
         "url": "https://www.caika.net/caika/",
-        agentId: '1806744140',
-        tenantCode: 'db_ck_ds2021',
+        agentId: '1751890535',
+        tenantCode: 'db_ck_qhdqm',
+        // 颜值时代生物科技有限公司
+        // "url": "https://www.caika.net/caika/",
+        // agentId: '1806744140',
+        // tenantCode: 'db_ck_ds2021',
         // 小丽都 西里食品
         // "url": "https://www.caika.net/caika/",
         // agentId: '1780714116',
